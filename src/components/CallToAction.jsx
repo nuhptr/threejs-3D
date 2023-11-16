66dbd5be5825873923a47facd7164b791bdf9c49
nuhptr <nugrahaadi733@gmail.com>
2023-11-16 10:46:57 +0700

    components/add CTA

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function CallToAction() {
+   return (
+      <section className="cta">
+         <p className="cta-text">
+            Have a project in mind? <br className="hidden sm:block" /> Let's build something together!
+         </p>
+         <Link to="/contact" className="btn">
+            Contact
+         </Link>
+      </section>
+   )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar"
 import Loader from "./Loader"
 import HomeInfo from "./HomeInfo"
 import Alert from "./Alert"
+import CallToAction from "./CallToAction"
 
-export { Navbar, Loader, HomeInfo, Alert }
+export { Navbar, Loader, HomeInfo, Alert, CallToAction }
