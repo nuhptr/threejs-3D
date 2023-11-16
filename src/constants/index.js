f22d5049c05f472ec7ff0b05cb2c74a8ed915451
nuhptr <nugrahaadi733@gmail.com>
2023-11-16 10:47:17 +0700

    constant/update code

--- src/constants/index.js
+++ src/constants/index.js
@@ -113,7 +113,7 @@ export const projects = [
       name: "Amazon Price Tracker",
       description:
          "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
-      link: "https://github.com/adrianhajdin/pricewise",
+      link: "https://github.com",
    },
    {
       iconUrl: threads,
@@ -121,7 +121,7 @@ export const projects = [
       name: "Full Stack Threads Clone",
       description:
          'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-      link: "https://github.com/adrianhajdin/threads",
+      link: "https://github.com",
    },
    {
       iconUrl: car,
@@ -129,7 +129,7 @@ export const projects = [
       name: "Car Finding App",
       description:
          "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-      link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+      link: "https://github.com",
    },
    {
       iconUrl: snapgram,
@@ -137,7 +137,7 @@ export const projects = [
       name: "Full Stack Instagram Clone",
       description:
          "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-      link: "https://github.com/adrianhajdin/social_media_app",
+      link: "https://github.com",
    },
    {
       iconUrl: estate,
@@ -145,7 +145,7 @@ export const projects = [
       name: "Real-Estate Application",
       description:
          "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-      link: "https://github.com/adrianhajdin/projects_realestate",
+      link: "https://github.com",
    },
    {
       iconUrl: summiz,
@@ -153,6 +153,6 @@ export const projects = [
       name: "AI Summarizer Application",
       description:
          "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-      link: "https://github.com/adrianhajdin/project_ai_summarizer",
+      link: "https://github.com",
    },
 ]
