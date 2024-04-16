a19b9d1c7023eb93ec4f82f145c8a28c6b39eb40
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 22:50:33 +0700

    assets: change name

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,59 +1,59 @@
-import css from './css.svg'
-import express from './express.svg'
-import git from './git.svg'
-import github from './github.svg'
-import html from './html.svg'
-import javascript from './javascript.svg'
-import mongodb from './mongodb.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
-import nextjs from './nextjs.svg'
-import nodejs from './nodejs.svg'
-import react from './react.svg'
-import redux from './redux.svg'
-import sass from './sass.svg'
-import tailwindcss from './tailwindcss.svg'
-import typescript from './typescript.svg'
-import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
-import car from './car.svg'
-import estate from './estate.svg'
-import pricewise from './pricewise.svg'
-import snapgram from './snapgram.svg'
-import summiz from './summiz.svg'
-import threads from './threads.svg'
-import arrow from './arrow.svg'
-import contact from './contact.svg'
-import soundon from './soundon.png'
-import soundoff from './soundoff.png'
+import css from "./ic_css.svg"
+import express from "./ic_express.svg"
+import git from "./ic_git.svg"
+import github from "./ic_github.svg"
+import html from "./ic_html.svg"
+import javascript from "./ic_javascript.svg"
+import mongodb from "./ic_mongodb.svg"
+import motion from "./ic_motion.svg"
+import mui from "./ic_mui.svg"
+import nextjs from "./ic_nextjs.svg"
+import nodejs from "./ic_nodejs.svg"
+import react from "./ic_react.svg"
+import redux from "./ic_redux.svg"
+import sass from "./ic_sass.svg"
+import tailwindcss from "./ic_tailwindcss.svg"
+import typescript from "./ic_typescript.svg"
+import linkedin from "./ic_linkedin.svg"
+import twitter from "./ic_twitter.svg"
+import car from "./ic_car.svg"
+import estate from "./ic_estate.svg"
+import pricewise from "./ic_pricewise.svg"
+import snapgram from "./ic_snapgram.svg"
+import summiz from "./ic_summiz.svg"
+import threads from "./ic_threads.svg"
+import arrow from "./ic_arrow.svg"
+import contact from "./ic_contact.svg"
+import soundon from "./img_soundon.png"
+import soundoff from "./img_soundoff.png"
 
 export {
-    css,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    react,
-    redux,
-    sass,
-    tailwindcss,
-    typescript,
-    linkedin,
-    twitter,
-    car,
-    estate,
-    pricewise,
-    snapgram,
-    summiz,
-    threads,
-    arrow,
-    contact,
-    soundon,
-    soundoff
+   css,
+   express,
+   git,
+   github,
+   html,
+   javascript,
+   mongodb,
+   motion,
+   mui,
+   nextjs,
+   nodejs,
+   react,
+   redux,
+   sass,
+   tailwindcss,
+   typescript,
+   linkedin,
+   twitter,
+   car,
+   estate,
+   pricewise,
+   snapgram,
+   summiz,
+   threads,
+   arrow,
+   contact,
+   soundon,
+   soundoff,
 }
