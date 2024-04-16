e11ec1eec59fb91f2428e45dbc2ef1035c2d679b
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 23:33:16 +0700

    hooks: change name

--- src/hooks/use-alert.js
+++ src/hooks/use-alert.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function useAlert() {
+export function useAlert() {
    const [alert, setAlert] = useState({ show: false, text: "", type: "danger" })
 
    const showAlert = ({ text, type = "danger" }) => setAlert({ show: true, text, type })
